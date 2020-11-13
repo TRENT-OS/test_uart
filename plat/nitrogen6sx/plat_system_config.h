1e7e9ec33a9c372e598e51ac20333db1c6401a7e
Axel Heider <axel.heider@hensoldt-cyber.de>
2020-11-13 13:42:41 +0100

    [DEV] add platform definition for Nitrogen Board

---
+++ plat/nitrogen6sx/plat_system_config.h
@@ -0,0 +1,10 @@
+/*
+ *  Platform defaults for the i.MX6 SoloX Nitrogen Board.
+ *
+ *  Copyright (C) 2020, Hensoldt Cyber GmbH
+ *
+*/
+
+
+// kernel log uses UART_0, so we can use UART_1 for i/o test
+#define UART_IO     UART_1
