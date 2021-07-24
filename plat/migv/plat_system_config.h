d740bc1ac592870a8248ef1f274697104232dd23
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-07-24 21:58:11 +0200

    [DEV] add support for MiG-V board

---
+++ plat/migv/plat_system_config.h
@@ -0,0 +1,8 @@
+/*
+ * Platform defaults for the MiG-V board.
+ *
+ * Copyright (C) 2021, Hensoldt Cyber GmbH
+ */
+
+// Kernel log uses UART_0, so we can use UART_1 for I/O tests.
+#define UART_IO     UART_1
