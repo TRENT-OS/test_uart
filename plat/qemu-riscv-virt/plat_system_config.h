4786ce72332d5713eac6d3f6a499b96f35f40774
Sebastian Eckl <sebastian.eckl@hensoldt.net>
2024-02-22 18:47:57 +0100

    [DEV] add support for qemu-riscv-virt

---
+++ plat/qemu-riscv-virt/plat_system_config.h
@@ -0,0 +1,10 @@
+/*
+ *  Platform defaults for the qemu-riscv-virt
+ *
+ *  Copyright (C) 2022, HENSOLDT Cyber GmbH
+ *
+*/
+
+
+// kernel log uses UART_0, so we can use UART_1 for i/o test
+#define UART_IO     UART_1
