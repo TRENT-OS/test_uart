40f35ea74d62d139754104430c9c17623d377c8a
Felix Schladt <felix.schladt@hensoldt.net>
2024-02-23 11:03:27 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- plat/jetson-nano-2gb-dev-kit/plat_system_config.h
+++ plat/jetson-nano-2gb-dev-kit/plat_system_config.h
@@ -1,9 +1,12 @@
 /*
  *  UART defaults for the platform jetson-nano-2gb-dev-kit (Nvidia Jetson Nano 2GB Developer Kit)
  *
- *  Copyright (C) 2022, HENSOLDT Cyber GmbH
+ *  Copyright (C) 2022-2024, HENSOLDT Cyber GmbH
+ * 
+ *  SPDX-License-Identifier: GPL-2.0-or-later
  *
-*/
+ *  For commercial licensing, contact: info.cyber@hensoldt.net
+ */
 
 
 // kernel log uses UART_0, so we can use UART_1 for i/o test
